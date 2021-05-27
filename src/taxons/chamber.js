3447ec5eb2c97af296f74b8ac85a09e913ef220b
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-05-27 14:12:40 +0200

    Update chambers.

--- src/taxons/chamber.js
+++ src/taxons/chamber.js
@@ -1,4 +1,15 @@
-const taxon = {};
+const taxon = {
+  civ1: 'Première chambre civile',
+  civ2: 'Deuxième chambre civile',
+  civ3: 'Troisième chambre civile',
+  comm: 'Chambre commerciale, économique et financière',
+  cr: 'Chambre criminelle',
+  soc: 'Chambre sociale',
+  mi: 'Chambre mixte',
+  pl: 'Assemblée plénière',
+  ordo: 'Première présidence (Ordonnance)',
+  creun: 'Chambres réunies',
+};
 
 module.exports = {
   options: [''].concat(Object.keys(taxon)),
