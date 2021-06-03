676fa869167ad4e4319985a8a2614bc1e2006fd9
antoinefa <fabien.antoine@m4x.org>
2021-06-03 22:11:42 +0200

    👷 start elasticsearch in CI

--- scripts/test_minimal.sh
+++ scripts/test_minimal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Test GET routes on ${SCHEME}://${HOST}:${API_PORT}
 for route in healthcheck search;do
-  if curl -s --retry 5 --retry-delay 2 -XGET ${SCHEME}://${HOST}:${API_PORT}/${route} | grep -q '"route":"GET /'${route}'"' ; then
+  if curl -s --retry 5 --retry-delay 2 -XGET ${SCHEME}://${HOST}:${API_PORT}/${route} | grep -q '"query":' ; then
       echo "✅ ${route}"
   else
       echo -e "\e[31m❌ ${route} !\e[0m"
