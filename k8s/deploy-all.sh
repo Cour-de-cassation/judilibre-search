02f5aa8d387aa300a40e379eddf2e7cfb75c50ec
antoinefa <fabien.antoine@m4x.org>
2021-06-07 01:04:18 +0200

    👷🐳☸️  retrofit from judilibre-admin, CI with k3s, CD with service PRA

--- k8s/deploy-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#es prerequisites
-kubectl apply -f https://download.elastic.co/downloads/eck/1.5.0/all-in-one.yaml
-for service in elasticsearch api ingress load-balancer;do
-	cat ${service}.yaml | envsubst | kubectl apply -f -;
-done
-
-#kubectl port-forward service/judilibre-elasticsearch-es-http 9200
-#curl -XPUT localhost:9200/${ELASTIC_INDEX}
