5dcecd9c2f7d76c83d05ccd82a2d6cdd0eb4de99
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-06-09 15:27:28 +0200

    Working on metadata. :fearful:

--- src/taxons/type.js
+++ src/taxons/type.js
@@ -1,9 +1,13 @@
+// @TODO info not in DB
+
 const taxon = {
   arret: 'Arrêt',
+  demande: "Demande d'avis",
   qpc: 'Question prioritaire de constitutionnalité',
   qpj: 'Question préjudicielle',
   ordonnance: 'Ordonnance',
   saisie: 'Saisie',
+  autre: 'Autre',
 };
 
 module.exports = {
