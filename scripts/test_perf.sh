ef94b5e3f261d348354f83ec542308126b49ec22
antoinefa <fabien.antoine@m4x.org>
2021-06-20 17:00:30 +0200

    ⚡️ add perf ID to keep env various reports

--- scripts/test_perf.sh
+++ scripts/test_perf.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#set version from package & git / could be git tag instead
+if [ -z "${VERSION}" ];then\
+        export VERSION="$(cat package.json | jq -r '.version')-$(git rev-parse --short HEAD)"
+fi
+
+export PERF_ID=${KUBE_ZONE}-${GIT_BRANCH}-${VERSION}_APP_${APP_NODES}_ES_${ELASTIC_NODES}_${ELASTIC_MEM_jvm}
+
 cat $(pwd)/scripts/perf/api_test.yaml | envsubst > $(pwd)/scripts/perf/api_test_configured.yaml
 
 mkdir -p $(pwd)/scripts/perf/reports
@@ -7,9 +14,9 @@ mkdir -p $(pwd)/scripts/perf/reports
 docker run --rm -it -v $(pwd)/scripts/perf:/scripts -v $(pwd)/scripts/perf/reports:/reports \
   --network host \
   artilleryio/artillery:latest \
-  run /scripts/api_test_configured.yaml --output /reports/report.json
+  run /scripts/api_test_configured.yaml --output /reports/report_${PERF_ID}.json
 
 docker run --rm -it -v $(pwd)/scripts/perf/reports:/reports \
   --network host \
   artilleryio/artillery:latest \
-  report /reports/report.json --output /reports/report.html
+  report /reports/report_${PERF_ID}.json --output /reports/report_${PERF_ID}.html
