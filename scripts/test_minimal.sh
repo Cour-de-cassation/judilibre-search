33826857da114fc0019b38964f17b9e462ab5e32
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-06-22 20:49:51 +0200

    Update minimal tests.

--- scripts/test_minimal.sh
+++ scripts/test_minimal.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 echo Test GET routes on ${APP_SCHEME}://${APP_HOST}:${APP_PORT}
-for route in healthcheck search;do
-  if curl -s -k --retry 5 --retry-delay 2 -XGET ${APP_SCHEME}://${APP_HOST}:${APP_PORT}/${route} | grep -q '"query":' ; then
-      echo "✅ ${route}"
-  else
-      echo -e "\e[31m❌ ${route} !\e[0m"
-      echo curl -k -XGET ${APP_SCHEME}://${APP_HOST}:${APP_PORT}/${route}
-      curl -k -XGET ${APP_SCHEME}://${APP_HOST}:${APP_PORT}/${route}
-      exit 1
-  fi
-done
+if curl -s -k --retry 5 --retry-delay 2 -XGET ${APP_SCHEME}://${APP_HOST}:${APP_PORT}/healthcheck | grep -q '"status":' ; then
+    echo "✅ healthcheck"
+else
+    echo -e "\e[31m❌ healthcheck !\e[0m"
+    echo curl -k -XGET ${APP_SCHEME}://${APP_HOST}:${APP_PORT}/healthcheck
+    curl -k -XGET ${APP_SCHEME}://${APP_HOST}:${APP_PORT}/healthcheck
+    exit 1
+fi
+if curl -s -k --retry 5 --retry-delay 2 -XGET ${APP_SCHEME}://${APP_HOST}:${APP_PORT}/search | grep -q '"results":' ; then
+    echo "✅ search"
+else
+    echo -e "\e[31m❌ search !\e[0m"
+    echo curl -k -XGET ${APP_SCHEME}://${APP_HOST}:${APP_PORT}/search
+    curl -k -XGET ${APP_SCHEME}://${APP_HOST}:${APP_PORT}/search
+    exit 1
+fi
