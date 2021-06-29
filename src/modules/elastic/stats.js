2b8118b1ce8fd98e1d5a0f48e9eafe00bd87c07b
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-06-29 11:32:18 +0200

    Add basic stats.

--- src/modules/elastic/stats.js
+++ src/modules/elastic/stats.js
@@ -6,17 +6,92 @@ async function stats(query) {
     return statsWithoutElastic.apply(this, [query]);
   }
 
-  let response = {};
+  let response = {
+    indexedTotal: 0,
+    requestPerDay: 0, // @TODO
+    requestPerWeek: 0, // @TODO
+    requestPerMonth: 0, // @TODO
+    oldestDecision: null,
+    newestDecision: null,
+    indexedByJurisdiction: [], // @TODO
+    indexedByYear: [], // @TODO
+  };
 
-  // @TODO
+  let stats = await this.client.indices.stats({
+    index: process.env.ELASTIC_INDEX,
+  });
+
+  if (
+    stats &&
+    stats.body &&
+    stats.body.indices &&
+    stats.body.indices[process.env.ELASTIC_INDEX] &&
+    stats.body.indices[process.env.ELASTIC_INDEX].total
+  ) {
+    stats = stats.body.indices[process.env.ELASTIC_INDEX].total;
+    if (stats.docs && stats.docs.count) {
+      response.indexedTotal = stats.docs.count;
+    }
+  }
+
+  let content = await this.client.search({
+    index: process.env.ELASTIC_INDEX,
+    size: 0,
+    body: {
+      aggs: {
+        min_date: {
+          min: {
+            field: 'decision_date',
+            format: 'yyyy-MM-dd',
+          },
+        },
+        max_date: {
+          max: {
+            field: 'decision_date',
+            format: 'yyyy-MM-dd',
+          },
+        },
+      },
+    },
+  });
+
+  if (content && content.body && content.body.aggregations) {
+    response.oldestDecision = content.body.aggregations.min_date.value_as_string;
+    response.newestDecision = content.body.aggregations.max_date.value_as_string;
+  }
 
   return response;
 }
 
 function statsWithoutElastic(query) {
-  let response = {};
-
-  // @TODO
+  let response = {
+    requestPerDay: 0,
+    requestPerWeek: 0,
+    requestPerMonth: 0,
+    indexedTotal: 0,
+    oldestDecision: '1800-01-01',
+    newestDecision: '2021-01-01',
+    indexedByJurisdiction: [
+      {
+        value: 1500000,
+        label: 'Cour de cassation',
+      },
+      {
+        value: 135000,
+        label: "Cour d'appel de Paris",
+      },
+    ],
+    indexedByYear: [
+      {
+        value: 250000,
+        label: '2019',
+      },
+      {
+        value: 195000,
+        label: '2018',
+      },
+    ],
+  };
 
   return response;
 }
