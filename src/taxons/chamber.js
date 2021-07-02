5d054ef8444c2634e2fb790328a8f57c5ffe028f
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-07-02 14:48:22 +0200

    Fix chambers order

--- src/taxons/chamber.js
+++ src/taxons/chamber.js
@@ -4,11 +4,11 @@ const taxon = {
   civ1: 'Première chambre civile',
   civ2: 'Deuxième chambre civile',
   civ3: 'Troisième chambre civile',
-  comm: 'Chambre commerciale, financière et économique',
+  comm: 'Chambre commerciale financière et économique',
   soc: 'Chambre sociale',
   cr: 'Chambre criminelle',
-  ordo: 'Première présidence (Ordonnance)',
   creun: 'Chambres réunies',
+  ordo: 'Première présidence (Ordonnance)',
 };
 
 module.exports = {
