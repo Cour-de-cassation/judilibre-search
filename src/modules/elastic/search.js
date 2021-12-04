a5125dc1344e95ef144cb76c9e59e42bad726fa1
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2021-12-04 20:44:55 +0100

    Bad idea

--- src/modules/elastic/search.js
+++ src/modules/elastic/search.js
@@ -118,7 +118,7 @@ async function search(query) {
                 rawResult.highlight[field].forEach(function (hit) {
                   hit = hit.replace(/^[^a-z<>]*/gim, '');
                   hit = hit.replace(/[^a-z<>]*$/gim, '');
-                  hit = hit.replace(/X+/gm, 'X…');
+                  hit = hit.replace(/X+/gm, '…');
                   result.highlights[key].push(hit.trim());
                 });
               }
@@ -134,7 +134,7 @@ async function search(query) {
                 rawResult.highlight[field + '.exact'].forEach(function (hit) {
                   hit = hit.replace(/^[^a-z<>]*/gim, '');
                   hit = hit.replace(/[^a-z<>]*$/gim, '');
-                  hit = hit.replace(/X+/gm, 'X…');
+                  hit = hit.replace(/X+/gm, '…');
                   result.highlights[key].push(hit.trim());
                 });
               }
