6e8691510b4f407bec826a98d379e7e35e433f06
antoinefa <fabien.antoine@m4x.org>
2022-01-14 20:36:16 +0100

    👷 move scripts deps to judilibre-ops

--- scripts/init_deps.sh
+++ scripts/init_deps.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-export DEPS_SRC=$(pwd)/judilibre-admin
+export GIT_OPS=judilibre-ops
+export DEPS_SRC=$(pwd)/${GIT_OPS}
 export SCRIPTS_SRC=${DEPS_SRC}/scripts
 export KUBE_SRC=${DEPS_SRC}/k8s
-export ELASTIC_SRC=${DEPS_SRC}/elastic
+export ELASTIC_SRC=$(pwd)/judilibre-admin/elastic
 
 # clone
-if [ ! -d ${SCRIPTS_SRC} ];then
-    if ! (git clone https://oauth2:${GIT_TOKEN}@github.com/Cour-de-cassation/judilibre-admin > /dev/null 2>&1); then
-        echo -e "\e[31m❌ init failed, couldn't clone git judilibre-admin repository \e[0m" && exit 1;
+if [ ! -d "${SCRIPTS_SRC}" ];then
+    if ! (git clone https://github.com/Cour-de-cassation/${GIT_OPS} > /dev/null 2>&1); then
+        echo -e "\e[31m❌ init failed, couldn't clone git ${GIT_OPS} repository \e[0m" && exit 1;
         if [ "${GIT_BRANCH}" == "master" ]; then
-            cd judilibre-admin;
+            cd ${SCRIPTS_SRC};
             git checkout master;
             cd ..;
         fi;
     fi;
 fi;
+
+if [ ! -d "${ELASTIC_SRC}" ];then
+    if ! (git clone https://github.com/Cour-de-cassation/judilibre-admin > /dev/null 2>&1); then
+        echo -e "\e[31m❌ init failed, couldn't clone git judilibre-admin repository \e[0m" && exit 1;
+        if [ "${GIT_BRANCH}" == "master" ]; then
+            cd ${SCRIPTS_SRC};
+            git checkout master;
+            cd ..;
+        fi;
+    fi;
+fi
 
 # scripts
 
