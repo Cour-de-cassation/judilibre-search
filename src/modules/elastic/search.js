ae66408d9c4f39ecb7225bff25e9e54f04910f8f
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2022-08-23 10:58:34 +0200

    Update search.js

--- src/modules/elastic/search.js
+++ src/modules/elastic/search.js
@@ -24,6 +24,10 @@ async function search(query) {
   };
 
   if (string && searchQuery.query) {
+    if (process.env.NODE_ENV !== 'production') {
+      response.searchQuery = searchQuery.query;
+    }
+
     let rawResponse = await this.client.search(searchQuery.query);
     if (rawResponse && rawResponse.body) {
       if (!rawResponse.body.hits || !rawResponse.body.hits.total || !rawResponse.body.hits.total.value) {
