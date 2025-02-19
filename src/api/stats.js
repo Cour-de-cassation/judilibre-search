61d414b277cb7866855f14d90d414748121a9016
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2025-02-19 09:19:58 +0100

    Update the other Swagger

--- src/api/stats.js
+++ src/api/stats.js
@@ -36,14 +36,14 @@ api.get(
     date_start: {
       in: 'query',
       isString: true,
-      isISO8601:true,
+      isISO8601: true,
       errorMessage: `Start date must be a valid ISO-8601 date (e.g. 2021-05-13).`,
       optional: true,
     },
     date_end: {
       in: 'query',
       isString: true,
-      isISO8601:true,
+      isISO8601: true,
       errorMessage: `End date must be a valid ISO-8601 date (e.g. 2021-05-13, 2021-05-13T06:00:00Z).`,
       optional: true,
     },
